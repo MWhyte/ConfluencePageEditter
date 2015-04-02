2e59b3a7cebd5c201d909b8ad2f3720b0aded506
Michael Whyte <mrmwhyte@gmail.com>
2015-04-03 00:30:38 +0100

    Setting Up Test Directory

---
+++ src/test/java/FakeTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created on 03/04/15.
+ */
+public class FakeTest {
+
+    @Test
+    public void fakeTestForNow(){
+        assertTrue(true);
+    }
+}
